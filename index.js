fbef9512b3513d64406c87baf00e297fe4e6dc24
GetteAlan <alanexequielgette@gmail.com>
2020-07-04 14:25:56 -0300

    simple node module

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-var rect = {
-    perimeter:(x,y)=>(2*(x+y)),
-    area:(x,y)=>(x*y)
-};
+const rectangle = require('./rectangle');
 
-function solveRect(l,b){
+function solveRect(l,b){              
     console.log('Solving for rectangle with l= ' + l + ' and b= ' + b);
 
     if(l <= 0 || b<= 0){
         console.log('Rectangle dimensions should be greater than zero. l= ' + l + ', and b= ' + b);
     }else{
-        console.log('The area of the rectangle is ' + rect.area(l,b));
-        console.log('The perimeter of the rectangle is ' + rect.perimeter(l,b));
+        console.log('The area of the rectangle is ' + rectangle.area(l,b));
+        console.log('The perimeter of the rectangle is ' + rectangle.perimeter(l,b));
     }
 }
 
@@ -18,5 +15,3 @@ solveRect(2,4);
 solveRect(3,5);
 solveRect(0,5);
 solveRect(-3,5);
-
-
