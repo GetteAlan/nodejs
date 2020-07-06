4730764fa7495f1b22017b783207379fa65e2479
GetteAlan <alanexequielgette@gmail.com>
2020-07-06 18:44:17 -0300

    express example

--- index.js
+++ index.js
@@ -1,53 +1,18 @@
+const express = require('express');
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const app = express();
 
 const hostname = 'localhost';
 const port = '3000';
 
-const server = http.createServer((req,res)=>{
-    console.log('Requst for ' + req.url + " by method " + req.method);
-
-    if(req.method == 'GET'){
-        var fileUrl = '';
-        if(req.url == '/'){
-            fileUrl = 'index.html';    
-        }else {
-            fileUrl = req.url;    
-        }
-        var filePath = path.resolve('./public/' + fileUrl);
-        const fileExt = path.extname(filePath);
-
-        if(fileExt == '.html'){
-            fs.exists(filePath, (exists) => {
-                if(!exists){
-                    res.statusCode = 404;
-                    res.setHeader('Content-Type','text/html');
-                    res.end('<html><body><h1>Error 404:' +  fileUrl 
-                        + ' not found</h1><body></html>');
-                    return;
-                } else {
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type','text/html');
-                    fs.createReadStream(filePath).pipe(res);
-                }
-            });
-        }else {
-            res.statusCode = 404;
-            res.setHeader('Content-Type','text/html');
-            res.end('<html><body><h1>Error 404:' +  fileUrl 
-                + ' not an HTML file</h1><body></html>');
-            return;
-        }
-    } else {
-        res.statusCode = 404;
-        res.setHeader('Content-Type','text/html');
-        res.end('<html><body><h1>Error 404:' +  res.method 
-            + ' not supported</h1><body></html>');
-        return;
-    }
+app.use((req,res,next)=> {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/html');
+    res.end('<html><body><h1>This is an express server</h1></body></html>');
 });
 
+const server = http.createServer(app);
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}`);
 });
